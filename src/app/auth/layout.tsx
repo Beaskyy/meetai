e9345df2fb4dd80fc3cecba28365672bd3f32398
Beaskyy <afeez.akintobi@yahoo.com>
2025-09-01 19:36:44 +0100

    update

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,6 +4,9 @@ const Signup = () => {
     <div className="flex justice items-center min-h-screen bg-amber-200">
       <h2>Sign Up</h2>
       <p>Enter your details to sign up</p>
+      <form>
+        <div>clear</div>
+      </form>
     </div>
   )
 }
