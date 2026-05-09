b5c2d414d88f6550b777dff39a422718255c1307
Beaskyy <afeez.akintobi@yahoo.com>
2026-05-09 22:21:20 +0100

    UI update

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,6 +1,9 @@
 const Page = () => {
   return ( 
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">sign up</div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <h2>Sign up</h2>
+      <p>Create your account</p>
+    </div>
    );
 }
  
